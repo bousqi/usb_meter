fe459e7cdd929522c6d82275270fecf09f424c61
Bousqi <bousqi@gmail.com>
2019-02-14 12:40:24 +0100

    added : - COM port listing - COM port check - Serial lost

--- usb_meter.py
+++ usb_meter.py
@@ -4,7 +4,8 @@ import sys
 import time
 import click
 import serial
-
+import serial.tools.list_ports
+from serial import tools
 from struct import unpack
 
 if sys.platform == 'win32':
@@ -12,6 +13,9 @@ if sys.platform == 'win32':
     init()
 
 def parse_packet(packet):
+    if len(packet) < 130:
+        return None
+
     data = unpack('>'+'H'*(len(packet)//2),packet)
     p_vcc  = (float)(data[1]/100)
     p_cur  = data[2]
@@ -38,8 +42,20 @@ def parse_packet(packet):
     return packet_val
 
 
+def is_COM_valid(port):
+    port_list = serial.tools.list_ports.comports()
+    name_list = [p.device for p in port_list]
+    return (port in name_list)
+
+
+def print_list():
+    port_list = serial.tools.list_ports.comports()
+    print (" Dev  - Name [ Desc ]")
+    for p in port_list:
+        print ("%s - %s [ %s ]"%(p.device, p.name, p.description))   
+
 @click.command()
-@click.option('--port', '-p', required=True,
+@click.option('--port', '-p', 
               help="COM Port to connect to USB Tester")
 @click.option('--baudrate', '-b',
               default=9600,
@@ -50,32 +66,60 @@ def parse_packet(packet):
 @click.option('--details', '-d',
               default=False, is_flag=True,
               help="Display all meter")
-def run(port, baudrate, refresh, details):
+@click.option('--list', '-l',
+              default=False, is_flag=True,
+              help="List all COM ports available")
+def run(port, baudrate, refresh, details, list):
+    if list:
+        print_list()
+        return
+
+    if port == None:
+        print ('Try "usb_monitor.py --help" for help.')
+        print ('Error: Missing option "--port" / "-p".\n')
+        print ('Use one of the port below :\n')
+        print_list()
+        return
+
+    if not is_COM_valid(port):
+        print ('%s port does not exists'%port)
+        print ('Use one of the port below :\n')
+        print_list()
+        return
+
     print ("Connecting to %s at %dbps"%(port, baudrate))
     z1serial = serial.Serial(port=port, baudrate=baudrate)
     z1serial.timeout = 2  # set read timeout
+
 
     minVcc = 10
     maxVcc = 0
     minCur = 1000
     maxCur = 0
 
-    print (z1serial.is_open)  # True for opened
+    # print (z1serial.is_open)  # True for opened
     if z1serial.is_open:
         while True:
-            z1serial.write(b'\x00')
-            data = z1serial.read(130)
+            try:
+                z1serial.write(b'\xF0')
+                data = z1serial.read(130)
+            except:
+                print("\n\nSerial Lost ?")
+                return
             # data_text = ''.join(['0x%02X ' % i for i in data])
             # print (data_text)
 
             state = parse_packet(data)
+            print("\033[2J", end='')
+            if state == None:
+                print('\rNo data...', end='')
+                continue
 
             maxVcc = state["Vcc"] if state["Vcc"] > maxVcc else maxVcc
             minVcc = state["Vcc"] if state["Vcc"] < minVcc else minVcc
             maxCur = state["Cur"] if state["Cur"] > maxCur else maxCur
             minCur = state["Cur"] if state["Cur"] < minCur else minCur
 
-            print("\033[2J", end='')
             print("%s : %.2fv (v%.2f ^%.2f)"%("Vcc", state["Vcc"], minVcc, maxVcc), end='')
             print(" - ", end='')
             print("%s : %4dmA (v%4d ^%4d)"%("Cur", state["Cur"], minCur, maxCur), end='')
